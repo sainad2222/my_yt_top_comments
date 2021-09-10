c4f9f87896f0cd0bd54ccb46740853c35a7fe35b
Sainath Singineedi <sainathsingineedi2222@gmail.com>
2021-09-10 18:30:15 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Usage:
+
+1. Clone this repository
+2. Go to [takeout](https://takeout.google.com/settings/takeout) and download
+   yoututbe takeout
+3. Copy "my_comments.html" from extracted takeout folder to current directory
+4. Install dependencies
+   `pip install -r requirements.txt`
+5. Generate Youtube Data API v3 key and replace it with 'API_KEY'
+6. Run main.py
+   `python main.py`
---
+++ main.py
@@ -0,0 +1,54 @@
+import os
+import googleapiclient.discovery
+from bs4 import BeautifulSoup
+from tqdm import tqdm
+
+"""
+params:
+    id: CommentId(String)
+returns:
+    -1, if error
+    likes count, otherwise
+"""
+
+
+def likes(id):
+    api_service_name = "youtube"
+    api_version = "v3"
+    DEVELOPER_KEY = os.getenv("API_KEY") or "INSERT_YOUR_API_KEY"
+
+    youtube = googleapiclient.discovery.build(
+        api_service_name, api_version, developerKey=DEVELOPER_KEY
+    )
+
+    request = youtube.comments().list(part="snippet", id=id)
+    try:
+        response = request.execute()
+    except Exception as e:
+        print(e, id)
+        return -1
+    if len(response["items"]) == 0:
+        return -1
+    try:
+        return response["items"][0]["snippet"]["likeCount"]
+    except Exception as e:
+        print(e, id)
+        return -1
+
+
+f = open("my-comments.html", "r")
+soup = BeautifulSoup(f, features="html.parser")
+comments = soup.find_all("li")
+result = []
+for comment in tqdm(comments):
+    commentLink = comment.a.attrs["href"]
+    commentContent = comment.contents[-1]
+    commentId = commentLink[commentLink.find("lc=") + 3 :]
+    k = likes(commentId)
+    if k == -1:
+        continue
+    result.append([k, commentLink, commentContent])
+result = sorted(result, reverse=True)
+# replace 10 with any number
+for likes, link, content in result[:10]:
+    print(likes, link, content)
